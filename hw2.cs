6e4d352a3f558d6634eabdc4801c1037cc7fcbe5
somard <somardk@gmail.com>
2024-03-07 09:45:49 -0500

    abc

---
+++ hw2.cs
@@ -0,0 +1,12 @@
+using System;
+using static System.Console;
+class Hw1 {
+	static void Main(){
+		WriteLine("Hello Hw2");
+		Wonderfun();
+	}
+
+	static void Wonderfun(){
+		WriteLine("Inside Wonderfun");
+	}
+}
